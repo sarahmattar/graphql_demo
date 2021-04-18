7ef982289c56c2776af37c695dddc5a6c8b4d80e
Sarah Mattar <sarah.mattar@mac.com>
2021-04-17 21:43:09 -0700

    Update rails to 6.0.3.5

--- app/graphql/graphql_fun_demo_schema.rb
+++ app/graphql/graphql_fun_demo_schema.rb
@@ -4,8 +4,8 @@ class GraphqlFunDemoSchema < GraphQL::Schema
 
   # Opt in to the new runtime (default in future graphql-ruby versions)
   use GraphQL::Execution::Interpreter
-  use GraphQL::Analysis::AST
+  # use GraphQL::Analysis::AST
 
   # Add built-in connections for pagination
-  use GraphQL::Pagination::Connections
+  GraphQL::Pagination::Connections
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_31_033657) do
+ActiveRecord::Schema.define(version: 2021_04_18_015156) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
